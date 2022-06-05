8edf3dfca244b2db0b5636accf2c30dce954008b
Glasgalas <alex.starichenko@icloud.com>
2022-06-05 13:13:14 +0300

    add routes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,12 +3,12 @@ import { Outlet } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyle';
 import styled from 'styled-components';
+import { Box } from '@chakra-ui/react';
 
 const List = styled.ul`
   list-style: none;
   display: flex;
   justify-content: space-around;
-  background-color: yellow;
   margin: 0;
   padding: 15px;
   border-bottom: 1px solid black;
@@ -25,34 +25,36 @@ export default function Layout() {
   return (
     <>
       <GlobalStyle />
-      <nav>
-        <List>
-          <ListItem>
-            <NavLink
-              to="contacts"
-              style={({ isActive }) => (isActive ? ActiveLink : undefined)}
-            >
-              Contacts
-            </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink
-              to="todos"
-              style={({ isActive }) => (isActive ? ActiveLink : undefined)}
-            >
-              Todos
-            </NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink
-              to="comments"
-              style={({ isActive }) => (isActive ? ActiveLink : undefined)}
-            >
-              Comments
-            </NavLink>
-          </ListItem>
-        </List>
-      </nav>
+      <Box w="100%" bgGradient="linear(to-r, gray.300, yellow.400, pink.200)">
+        <nav>
+          <List>
+            <ListItem>
+              <NavLink
+                to="contacts"
+                style={({ isActive }) => (isActive ? ActiveLink : undefined)}
+              >
+                Contacts
+              </NavLink>
+            </ListItem>
+            <ListItem>
+              <NavLink
+                to="todos"
+                style={({ isActive }) => (isActive ? ActiveLink : undefined)}
+              >
+                Todos
+              </NavLink>
+            </ListItem>
+            <ListItem>
+              <NavLink
+                to="comments"
+                style={({ isActive }) => (isActive ? ActiveLink : undefined)}
+              >
+                Comments
+              </NavLink>
+            </ListItem>
+          </List>
+        </nav>
+      </Box>
       <Outlet />
     </>
   );
